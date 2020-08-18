12a16fbc1386e23429105098791c2369ef2704e9
max3252 <68981701+max3252@users.noreply.github.com>
2020-08-18 11:52:09 +0900

    バリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,34 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: "Full-width characters" } do
+    validates :family_name
+    validates :first_name
+  end
+
+  with_options presence: true, format: { with: /\A[\p{katakana}　ー－&&[^ -~｡-ﾟ]]+\z/, message: "Full-width katakana characters" } do
+    validates :family_name_kana
+    validates :first_name_kana
+  end
+
+  validates :nickname,
+    presence: true
+
+  validates :email,
+    uniqueness: true
+
+  validates :password,
+    length: { minimum: 6 },
+    confirmation: true,
+    format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: "Include both letters and numbers" }
+  
+  validates :password_confirmation, 
+    presence: true
+
+  validates :birth_day,
+    presence: true
+
+  
 end

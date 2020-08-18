2c1d2c3bfd2cece864640be18716de2dd52ca1c3
max3252 <68981701+max3252@users.noreply.github.com>
2020-08-18 19:09:20 +0900

    itemsテーブルの作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,26 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_17_014142) do
+ActiveRecord::Schema.define(version: 2020_08_18_095212) do
+
+  create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name", null: false
+    t.text "text", null: false
+    t.string "image", null: false
+    t.integer "price", null: false
+    t.integer "category", null: false
+    t.integer "status", null: false
+    t.integer "prefecture", null: false
+    t.integer "burden", null: false
+    t.integer "days", null: false
+    t.bigint "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_items_on_user_id"
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "nickname", null: false
+    t.string "nickname", default: "", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "family_name", null: false
@@ -30,4 +46,5 @@ ActiveRecord::Schema.define(version: 2020_08_17_014142) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "items", "users"
 end

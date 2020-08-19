eb2ba9e800076d850d0f53dc197428e794d9d243
max3252 <68981701+max3252@users.noreply.github.com>
2020-08-19 14:34:01 +0900

    Active Hashを導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,7 +4,21 @@ class ItemsController < ApplicationController
   end
 
   def new
-    @items = Item.all
+    @item = Item.new
   end
 
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def article_params
+    params.require(:item).permit(:title,:text,:genre_id)
+  end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,13 @@
 class Item < ApplicationRecord
   has_one_attached :image
+  
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :category, :status, :burden, :prefecture, :days
 
+  
+  validates :title, :text, :category, :status, :burden, :prefecture, :days, presence: true
+
+  
+  validates :category_id, :status_id, :burden_id, :purefecture_id, :days_id, numericality: { other_than: 1 } 
+ 
 end

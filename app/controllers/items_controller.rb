67fe87d31731ba18ef304b1c340b9fe74a877635
max3252 <68981701+max3252@users.noreply.github.com>
2020-08-22 11:11:43 +0900

    商品詳細表示機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,6 @@
 class ItemsController < ApplicationController
+  before_action :move_to_index, except: [:index, :show]
+
   def index
     @items = Item.all
     @items = Item.includes(:user).order('created_at DESC')
@@ -17,9 +19,20 @@ class ItemsController < ApplicationController
     end
   end
 
+  def show
+    @item = Item.find(params[:id])
+    end
+
   private
 
   def item_params
     params.require(:item).permit(:image, :name, :text, :price, :category_id, :status_id, :burden_id, :prefecture_id, :day_id).merge(user_id: current_user.id)
   end
+
+  def move_to_index
+    unless user_signed_in?
+      redirect_to action: :index
+    end
+  end
+  
 end
